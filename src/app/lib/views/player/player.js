7908e0afc50538cd839230ad1d092ea5c44fd236
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2021-08-15 16:44:41 +0300

    added in core

--- src/app/lib/views/player/player.js
+++ src/app/lib/views/player/player.js
@@ -545,7 +545,6 @@
                         //biggerSubtitle: {},
                         //smallerSubtitle: {},
                         //customSubtitles: {},
-                        //progressTips: {}
                     }
                 });
                 this.video.ready(function () {
